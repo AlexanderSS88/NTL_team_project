acd5a582df94e8bc1dc74cdeb27670e55ff1fdd3
unknown <alex6594@mail.ru>
2022-07-07 22:04:06 +0300

    first db commit

---
+++ DB.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS user_info (
+id integer PRIMARY KEY,
+user_name varchar(40) NOT NULL,
+user_surname varchar(40) NOT NULL,
+account_link varchar(80) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS photos (
+id integer PRIMARY KEY,
+photo_link varchar(300),
+photo_id_user integer REFERENCES user_info(id)
+)
+;
+CREATE TABLE IF NOT EXISTS interests(
+id integer PRIMARY KEY,
+interest_name varchar(40) NOT NULL,
+interest_id_user integer REFERENCES user_info(id)
+);
