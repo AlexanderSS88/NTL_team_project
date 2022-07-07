9b371c8b2234df7877f39a34981e0e85b080dfc5
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-07-07 17:16:52 +0100

    initial commit

---
+++ cls/cls_VkUrl.py
@@ -0,0 +1,69 @@
+import requests
+from cls.cls_HttpReq import HttpR
+
+from pprint import pprint
+
+"""
+This is the VKontakte API communication class
+"""
+
+
+class VkUrl(HttpR):
+    url_ = "https://api.vk.com/method/"
+
+    def __init__(self, token_file_n: str):
+        super().__init__(token_file_n)
+
+    def get_url(self, method: str):
+        """
+        This method just merge an default url and http method name.
+        """
+        return self.url_ + method
+
+    def get_params(self, fields: str, pdict: dict):
+        """
+        This method just merge http request parameters.
+        """
+        return {'access_token': self.token,
+                'v': '5.81',
+                'fields': fields} | pdict
+
+    @staticmethod
+    def transform_param(param: dict):
+        return ''.join([f'&{key}={value}' for key, value in param.items()])
+
+    def get_personal_data(self, user_id: str) -> dict:
+        """
+        Gets a user's data by user id
+        """
+        result = requests.get(self.get_url(method="users.get"),
+                              params=self.transform_param(
+                                  self.get_params(
+                                      fields='photo_400_orig,'
+                                             'interests,'
+                                             'music',
+                                      pdict={'user_ids': user_id})
+                              ), timeout=5)
+
+        return result.json()
+
+    def get_photo_f_profile(self, user_id: str, album_name: str) -> dict | str:
+
+        """
+        Gets a user's photos data by user id
+        """
+        result = requests.get(self.get_url(method="photos.get"),
+                              params=self.transform_param(
+                                  self.get_params(
+                                      fields='',
+                                      pdict={'owner_id': user_id,
+                                             'album_id': album_name,
+                                             'count': '200',
+                                             'photo_sizes': '1',
+                                             'extended': '1'})),
+                              timeout=5)
+
+        if result.status_code == 200 and 'error' not in result.json():
+            return result.json()['response']['items']
+        else:
+            return f"Error"
---
+++ main.py
@@ -0,0 +1,11 @@
+from pprint import pprint
+
+from cls.cls_VkUrl import VkUrl
+
+if __name__ == '__main__':
+    vk = VkUrl('vk_token.txt')
+    vk_id = '668524'
+
+    pprint(vk.get_personal_data(user_id=vk_id))
+    pprint(vk.get_photo_f_profile(user_id=vk_id, album_name='wall'))
+    pprint(vk.get_photo_f_profile(user_id=vk_id, album_name='profile'))
