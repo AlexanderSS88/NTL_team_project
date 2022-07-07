2f36cea175052db9e81ae61cb2acb849502e6a02
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-07-07 17:17:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from cls.cls_VkUrl import VkUrl
 
 if __name__ == '__main__':
     vk = VkUrl('vk_token.txt')
-    vk_id = '668524'
+    vk_id = '1'
 
     pprint(vk.get_personal_data(user_id=vk_id))
     pprint(vk.get_photo_f_profile(user_id=vk_id, album_name='wall'))
