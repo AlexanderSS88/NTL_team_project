d2bf462e305238ed83a44f0222afda8207adfe8f
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-07-10 20:34:06 +0100

    fixed database cfg

--- cls/cls_DataBaseExchange.py
+++ cls/cls_DataBaseExchange.py
@@ -1,54 +1,42 @@
 import sqlalchemy
 import pathlib
 from pathlib import Path
-import configparser  # импортируем библиотеку
+import configparser
 
 
 class DataBaseExchange:
 
-    # def __init__(self, db_info='postgresql://postgres:vrag@localhost:5432/courcr_w_DB'):
     def __init__(self, db_data_file_path='/tokens/database_data.txt'):
         self.connection = self.make_database_connection(db_data_file_path)
 
         print(self.connection)
 
     def make_database_connection(self, db_data_file_path: str):
-
         path = str(Path(pathlib.Path.cwd())) + db_data_file_path
         print(pathlib.Path.cwd())
         print(path)
-
 
         config = configparser.ConfigParser()  # create a parser object
         print(path)
         print(config.read(path))  # read confif file
 
-        # # print(config)
-        # print(len(config))
-        # print(config['DEFAULT'])
-        #
-        # for key, val in config['DEFAULT'].items():
-        #     print(f'{key}: {val}')
-        #
-        # user_name = config['DEFAULT']['user_name']
-        # user_pass_word = config['DEFAULT']['user_pass_word']
-        # port = config['DEFAULT']['port']
-        # database_name = config['DEFAULT']['database_name']
-        # host = config['DEFAULT']['host']
-        #
-        # # engine = sqlalchemy.create_engine('postgresql://postgres:vrag@localhost:5432/cours_w_DB')
-        #
-        # engine = sqlalchemy.create_engine(
-        #     f'postgresql://{user_name}:{user_pass_word}@{host}:{port}/{database_name}')
-        #
-        # engine = sqlalchemy.create_engine(
-        #     f'postgresql://{user_name}:{user_pass_word}@{host}:{port}/{database_name}')
-        #
-        # return engine.connect()
+        # print(config)
+        print(len(config))
+        print(config['DEFAULT'])
 
-        return None
+        for key, val in config['DEFAULT'].items():
+            print(f'{key}: {val}')
 
+        user_name = config['DEFAULT']['user_name']
+        user_pass_word = config['DEFAULT']['user_pass_word']
+        port = config['DEFAULT']['port']
+        database_name = config['DEFAULT']['database_name']
+        host = config['DEFAULT']['host']
 
+        engine = sqlalchemy.create_engine(
+            f'postgresql://{user_name}:{user_pass_word}@{host}:{port}/{database_name}')
+
+        return engine.connect()
 
     def create_tables(self):
         sel = self.connection.execute("""
