099ec8c696106c31d22c2051a8f23d045df0da79
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-07-14 22:28:49 +0100

    some tests added

--- test/test_DataBaseExchange.py
+++ test/test_DataBaseExchange.py
@@ -1,8 +1,33 @@
 import unittest
+from unittest import mock
 from parameterized import parameterized
 from tokens.cls_tokens import Token
 
 from cls.cls_DataBaseExchange import DataBaseExchange
+
+# This method will be used by the mock to replace requests.get
+def mocked_connection_execute(*args):
+    class MockResponse:
+        def __init__(self, db_return):
+            self.db_return = db_return
+
+        def response(self):
+            return self.db_return
+
+    print(f'args:\t{args}')
+
+    if args == "SELECT id FROM user_info WHERE age = 30 AND city = 'Москва';":
+        return MockResponse([1, 2])
+    # elif args[0] == 'https://api.vk.com/method/photos.get' \
+    #         and kwargs[
+    #     'params'] == '&access_token=vk_token&v=5.81&fields=&owner_id=125&album_id=profile&count=200&photo_sizes' \
+    #                  '=1&extended=1':
+    #     return MockResponse({"response": {"items": "value2"}}, 200)
+    # elif args[0] == 'https://api.vk.com/method/photos.getAlbums' \
+    #         and kwargs['params'] == '&access_token=vk_token&v=5.81&fields=&owner_id=125':
+    #     return MockResponse({'response': {'items': [{'id': 1000}]}}, 200)
+
+    return MockResponse(None)
 
 
 class TestDBFunctions(unittest.TestCase):
@@ -23,6 +48,12 @@ class TestDBFunctions(unittest.TestCase):
     def test_prepare_database_connection(self):
         self.assertMultiLineEqual(self.db.prepare_database_connection(),
                                   f'postgresql://postgres:password@localhost:5432/cours_w_DB')
+
+    @mock.patch('cls.cls_DataBaseExchange.connection.execute', side_effect=mocked_connection_execute)
+    def test_get_candidates(self, mock_get):
+        data = self.db.create_tables()
+        self.assertListEqual(data, [1, 2])
+
 
 
 
--- test/test_VK.py
+++ test/test_VK.py
@@ -15,9 +15,6 @@ def mocked_requests_get(*args, **kwargs):
         def json(self):
             return self.json_data
 
-    print(f'args[0]:\t{args[0]}')
-    pprint(f'kwargs: {kwargs}')
-
     if args[0] == 'https://api.vk.com/method/users.get' \
             and kwargs['params'] == '&access_token=vk_token&v=5.81&fields=bdate,sex,city,interests,music&user_ids=125':
         return MockResponse({"response": "value1"}, 200)
@@ -56,23 +53,19 @@ class TestVKFunctions(unittest.TestCase):
     @mock.patch('vk_tools.cls_VkUrl.requests.get', side_effect=mocked_requests_get)
     def test_get_personal_data(self, mock_get):
         json_data = self.vk.get_personal_data('125')
-        print(f'json_data: {json_data}')
         self.assertEqual(json_data, {"response": "value1"})
 
     @mock.patch('vk_tools.cls_VkUrl.requests.get', side_effect=mocked_requests_get)
     def test_get_photo_f_profile(self, mock_get):
         json_data = self.vk.get_photo_f_profile('125', 'profile')
-        print(f'json_data2: {json_data}')
         self.assertEqual(json_data, "value2")
 
     @mock.patch('vk_tools.cls_VkUrl.requests.get', side_effect=mocked_requests_get)
     def test_get_photo_f_profile_by_album_list(self, mock_get):
         json_data = self.vk.get_photo_f_profile_by_album_list('125', ['profile'])
-        print(f'json_data2: {json_data}')
         self.assertListEqual(json_data, ["value2"])
 
     @mock.patch('vk_tools.cls_VkUrl.requests.get', side_effect=mocked_requests_get)
     def test_search_albums(self, mock_get):
         json_data = self.vk.search_albums('125')
-        print(f'json_data2: {json_data}')
         self.assertListEqual(json_data, [1000])
