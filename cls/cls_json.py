2d3761e61e0554c47bba6782f8a65c79730c5026
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-07-19 12:34:53 +0100

    added get_candidates_from_json and get_candidate_data_from_json

--- cls/cls_json.py
+++ cls/cls_json.py
@@ -1,35 +1,25 @@
 import json
 from pprint import pprint
 
-class Add2Json():
+
+class Add2Json:
 
     def __init__(self, file_name):
         self.file_name = file_name
-        # json_base = {}
-        # with open(self.file_name, "w") as f:
-        #     json.dump(json_base, f, ensure_ascii=False, indent=3)
 
     def add_2_json(self, user_id: str, user_dict: dict):
         user_json = {user_id: user_dict}
-
-        print('\nuser_dict:')
-        pprint(user_dict)
 
         second_json = {}
 
         with open(self.file_name, encoding="utf-8", errors="ignore") as f:
             second_json = json.load(f)
-            print('\nsecond_json1:')
-            pprint(second_json)
 
         if str(user_id) in second_json.keys():
             print('User is in json yet.')
 
         else:
             second_json.update(user_json)
-
-            print('\nsecond_json2:')
-            pprint(second_json)
 
             try:
                 with open(self.file_name, "w", encoding="utf-8") as f2:
@@ -38,3 +28,25 @@ class Add2Json():
                 print("Codec can't decode some byte. Data wasn't recorded in json.")
 
             print('User data saved in json.')
+
+    def get_candidates_from_json(self, min_age: int, max_age: int, city_name: str) -> list:
+
+        with open(self.file_name, encoding="utf-8", errors="ignore") as f:
+            data_json = json.load(f)
+
+            candidate_list = []
+
+        for user_id, user_data in data_json.items():
+            if min_age <= user_data['age'] <= max_age and user_data['city'] == city_name:
+                candidate_list.append(user_id)
+
+        return candidate_list
+
+    def get_candidate_data_fron_json(self, user_id):
+        with open(self.file_name, encoding="utf-8", errors="ignore") as f:
+            data_json = json.load(f)
+            print('\ndata_json:')
+            pprint(data_json)
+
+        return data_json[str(user_id)], data_json[str(user_id)]['photos_id_list']
+
